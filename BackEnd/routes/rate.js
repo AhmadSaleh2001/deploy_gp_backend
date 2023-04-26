3dda61e688a5edae43bf85d8b7d6b9bbb1ebe14f
Ahmad Saleh <79485253+AhmadSaleh2001@users.noreply.github.com>
2023-04-27 01:28:49 +0300

    Delete BackEnd directory

--- BackEnd/routes/rate.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express").Router();
-const rateController = require("../controller/rateController");
-let { isAuth } = require("../helper/Auth");
-Router.get("/getAll", isAuth, rateController.getAll);
-Router.post("/add", rateController.add);
-module.exports = Router;
--- BackEnd/routes/url.js
+++
@@ -1,5 +0,0 @@
-const Router = require("express").Router();
-const urlController = require("../controller/urlController");
-Router.get("/getAll", urlController.getAll);
-Router.post("/add", urlController.add);
-module.exports = Router;
